426ea7c38bc25e629e5acc249af86f0d0d5ddf78
MAYANK TIWARI <gojogaming623@gmail.com>
2025-02-17 12:48:34 +0530

    Add files via upload

---
+++ js/imagelink.js
@@ -0,0 +1,9 @@
+function openPopup(src) {
+    document.getElementById("popup-container").style.display = "block";
+    document.getElementById("popup-image").src = src;
+  }
+  
+  function closePopup() {
+    document.getElementById("popup-container").style.display = "none";
+  }
+  
